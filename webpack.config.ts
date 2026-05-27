96c217866523790898db7e27cf304c003ed5b79b
Leandro Silva <lleandro.adal@gmail.com>
2026-05-27 16:28:52 -0300

    chore: add webpack config

---
+++ webpack.config.ts
@@ -0,0 +1,33 @@
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import webpack from 'webpack';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const config: webpack.Configuration = {
+  mode: 'development',
+  entry: './src/19 - webpack/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          configFile: 'tsconfig.frontend.json',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'frontend', 'assets', 'js'),
+  },
+  devtool: 'source-map',
+};
+
+export default config;
