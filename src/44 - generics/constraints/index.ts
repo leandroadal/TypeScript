00595cd8d8e474c75b6b4558073169b693c9df76
Leandro Silva <lleandro.adal@gmail.com>
2026-05-29 03:27:14 -0300

    feat: add generic constraints examples

---
+++ src/44 - generics/constraints/index.ts
@@ -0,0 +1,18 @@
+// O extends serve com forma de restringir o espoco
+// do genérico quando usado no contexto de definição de tipo
+
+type ObterChaveFn = <O, K extends keyof O>(objeto: O, chave: K) => O[K];
+
+const obterChave: ObterChaveFn = (objeto, chave) => objeto[chave];
+
+const animal = {
+  cor: 'Rosa',
+  vacinas: ['Vacina 1', 'Vacina 2'],
+  idade: 10,
+};
+
+// K so pode ser uma chave do objeto, nesse caso, animal
+const vacinas = obterChave(animal, 'vacinas');
+const cor = obterChave(animal, 'cor');
+
+console.log(vacinas, cor, obterChave(animal, 'idade'));
